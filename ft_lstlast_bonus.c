f6b172fdf0c0f2d6ad75fbd64b0c2d6846a30eee
feberman42 <feberman@student.42berlin.de>
2023-05-08 14:09:43 +0200

    Fixed support for empty input

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 18:05:06 by feberman          #+#    #+#             */
-/*   Updated: 2023/05/07 18:07:49 by feberman         ###   ########.fr       */
+/*   Updated: 2023/05/08 14:09:21 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst->next != 0)
-		lst = lst->next;
+	if (lst != 0)
+	{
+		while (lst->next != 0)
+			lst = lst->next;
+	}
 	return (lst);
 }
